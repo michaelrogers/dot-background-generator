37310176b9f8389e090d4a8f203d110264be2808
michaelrogers <contact@mhrogers.com>
2016-08-24 08:56:37 -0500

    Added a timeout on resize events to limit the number of redraws when resizing the window
    Features:
    + Added timeoutResize to delay the redraw of the dots by 200ms after the
    canvas has been resized

--- canvas.js
+++ canvas.js
@@ -1,10 +1,11 @@
 var backgroundLayer, context;
-var skewDegrees = 17; skewDegrees /= 100;
+var skewDegrees = 7; skewDegrees /= 100;
 var dotSpacing = 1, dotMaxRadius = 8, dotCenterDistance = dotSpacing + dotMaxRadius*2;
 var dotOpacity = 0.8;
+var timeoutResize;
+var intSet; var intervalTimes;
 
-window.onload = function (){init();}
-
+window.onload = function (){init()}
 
 function getCanvasContext(){
 	backgroundLayer = document.getElementById("backgroundCanvas");
@@ -13,9 +14,21 @@ function getCanvasContext(){
 
 function init(){
 	getCanvasContext();
-	window.addEventListener("resize", resizeCanvas);
+	document.title = "Dot Background Generator";
+	window.addEventListener("resize", timeoutResize);
 	resizeCanvas();
+	// intervalTimes = 40;
+	// intSet = setInterval(skewRotate, 50);
+	
 }
+
+function skewRotate(){
+	resizeCanvas();
+	intervalTimes-=1;
+	skewDegrees += 0.2 / 100;
+	if(intervalTimes<1){clearInterval(intSet)};
+}
+
 function drawBackground(){
 	var color1 = 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')';
 	var color2 = 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')';
@@ -23,7 +36,14 @@ function drawBackground(){
 	backgroundLayer.style.background="linear-gradient(to top,"+color1+","+color2+")";
 }
 
+function timeoutResize(){
+	clearTimeout(timeoutResize);
+	timeoutResize = setTimeout(function(){resizeCanvas()},200);
+}
+
 function resizeCanvas(){
+
+	console.count("resizeCanvas");
 	backgroundLayer.width = window.innerWidth;
 	backgroundLayer.height = window.innerHeight;
 	console.log({width: backgroundLayer.width, height: backgroundLayer.height});
@@ -37,7 +57,7 @@ function dotsLoop () {
 	var color2 = 'rgba('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+dotOpacity+')';
 	console.log({dotsColor1: color1, dotsColor2: color2});
 	
-	decrementVariable = 100 * (dotCenterDistance)/ window.innerHeight; //Determine what percentage to decrement each turn
+	decrementVariable = 105 * (dotCenterDistance)/ window.innerHeight; //Determine what percentage to decrement each turn
 	// decrementVariable = Math.ceil(window.innerHeight * Math.tan(skewDegrees)); 
 
 	var oppositeSideHeight = (dotCenterDistance) * Math.tan(skewDegrees); //Use tangent to determine the opposite since the adjacent and angle are given
@@ -48,11 +68,10 @@ function dotsLoop () {
 		for (let y=0; y < Math.ceil(window.innerHeight * Math.tan(skewDegrees)); y++){ 
 		
 			var dotColor; oddRowSpacing=0;
-			if (y % 2 == 0){dotColor = color1; oddRowSpacing = (dotCenterDistance)/2}
+			if (y % 2 == 0){dotColor = color1; oddRowSpacing = (dotCenterDistance - dotMaxRadius)}
 			else {dotColor = color2; oddRowSpacing = 0;}
 
-
-		drawDots((dotCenterDistance)*x -oppositeSideHeight*x + oddRowSpacing*Math.cos(skewDegrees), //x
+			drawDots((dotCenterDistance)*x - oppositeSideHeight*x + oddRowSpacing*Math.cos(skewDegrees), //x
 		 		(dotCenterDistance)*y - oppositeSideHeight*x - oddRowSpacing*Math.tan(skewDegrees), //Y
 		 		dotMaxRadius * (1-(decrementVariable*(y)/130)), //radius
 				// dotMaxRadius * (1-(decrementVariable*(y-(dotCenterDistance * Math.tan(skewDegrees/100)))/100)), //radius
